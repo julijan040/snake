c96917ce3b430a95cf88a5e4bc90a585f70518a1
julijan040 <js1840@student.uni-lj.si>
2016-04-01 11:59:12 +0200

    mapMaker

---
+++ snake/Assets/scripts/mapMaker.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapMaker : MonoBehaviour {
+
+    public GameObject tile;
+
+    public int width; // width of map
+    public int height; // height of map
+    public int startPositionX; // start X position of tiles
+    public int startPositionY; // start Y position of tiles    
+    int originalStartPositionX;
+
+    public Transform canvasTransform;
+
+    void Start ()
+    {
+        makeMap();	    
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    public void makeMap()
+    {
+        originalStartPositionX = startPositionX;
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                GameObject copyTile = (GameObject)Instantiate(tile, new Vector2(startPositionX, startPositionY), Quaternion.identity);
+                copyTile.transform.SetParent(canvasTransform, false);
+                startPositionX+=50;
+            }
+            startPositionX = originalStartPositionX;
+            startPositionY-=50;
+        }
+    }
+}
